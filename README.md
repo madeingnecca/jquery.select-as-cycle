b143114f3aa95ba3c1bb582c167e8ff688cbfc6a
Damiano Seno <dax86it@gmail.com>
2013-01-25 00:54:43 +0100

    first significant plugin

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-jquery.select-as-cycle
-======================
+This is yet another plugin for decorating select elements, but this time no fancy dropdown is involved. Instead it's given the possibility to navigate their options via prev/next commands.
+The continuous navigation is provided by jquery.cycle plugin; all cycle options can be specified for customized effects.
+
+A demo showing how it works and with a basic API usage, can be found at: http://codepen.io/madeingnecca/pen/mnCjq
---
+++ jquery.select-as-cycle.js
@@ -0,0 +1,90 @@
+(function($, undefined){
+  $.fn.selectAsCycle = function(opts) {
+    opts = $.extend({}, opts || {});
+    var EMPTY_FN = function() {};
+
+    var prev = $.extend({
+      text: 'Prev',
+      pos: 'after'
+    }, opts.prev || {});
+
+    var next = $.extend({
+      text: 'Next',
+      pos: 'before'
+    }, opts.next || {});
+
+    var changeOn = opts.changeOn || 'after';
+    var init = opts.init || EMPTY_FN;
+    var renderItem = opts.renderItem || function($opt) {
+      return $opt.text();
+    };
+
+    $(this).each(function() {
+      var $select = $(this);
+      var $container = $('<div class="select-as-cycle-container"></div>');
+      var $content = $('<ul class="select-as-cycle-content"></ul>');
+      var $prev = $('<div class="select-as-cycle-prev">' + prev.text + '</div>');
+      var $next = $('<div class="select-as-cycle-next">' + next.text + '</div>');
+
+      $('option', $select).each(function(index) {
+        var $opt = $(this);
+        var $item = $('<li class="select-as-cycle-item">' + renderItem($opt) + '</li>');
+        $item.data('select-as-cycle-index', index);
+        $item.appendTo($content);
+      });
+
+      $select.after($container);
+      $content.appendTo($container);
+      $content[prev.pos]($prev);
+      $content[next.pos]($next);
+
+      var cycleOpts = $.extend({
+        fx: 'scrollVert',
+        timeout: 0
+      }, opts.cycle || {});
+
+      var cb = cycleOpts[changeOn] || EMPTY_FN;
+      cycleOpts.prev = $prev;
+      cycleOpts.next = $next;
+      cycleOpts.startingSlide = $select[0].selectedIndex;
+
+      cycleOpts[changeOn] = function(curr, next, opts) {
+        cb(curr, next, opts);
+
+        var index = $(next).data('select-as-cycle-index');
+        $select[0].selectedIndex = index;
+        $select.trigger('change', [true]);
+      };
+
+      $select.change(function(ev, cycled) {
+        if (!cycled) {
+          // Do not change slide if change was triggered by 'changeOn'.
+          var index = this.selectedIndex;
+          $content.cycle(index);
+        }
+      });
+
+      $content
+      .cycle(cycleOpts);
+
+      if (opts.wheel && 'mousewheel' in $.fn) {
+        $content
+        .mousewheel(function(event, delta) {
+          if (delta > 0 && next.pos == 'before') {
+            $next.click();
+          }
+          else {
+            $prev.click();
+          }
+        });
+      }
+
+      $select.addClass('select-as-cycle');
+
+      // Call custom initializer.
+      init.call($select, $content, $next, $prev, opts);
+    });
+
+    return $(this);
+  };
+}(jQuery));
